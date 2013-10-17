e948cfac709151925d09cfa58fae42344d3c24ef
Sri Kadimisetty <s@sri.io>
2013-10-17 17:09:58 -0500

    customise calendar fit to inputs size

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,46 @@
 angular.module('SimpleWorkCal', [])
-.directive('datepicker', function($parse) {
+.factory('tasks', function() {
+	return [ 'Buy some milk', 
+				'something here',
+				'Visit the bank',
+				'Pay bills',
+				'Walk the monkey',
+				'make some chilli',
+				'pack dinner',
+				'wash car',
+				'brush teeth' ];
+})
 
-	var linker = function(scope, element, attrs) {
+.controller('DateCtrl', function($scope, tasks) {
+	var currentDate = new Date(); // Lets begin with today!!
+	$scope.chosenDate = currentDate;
+	$scope.tasks = tasks;
+	// $scope.updateScope = function() {
+	// }
+	$scope.safeApply = function(fn) {
+		var phase = this.$root.$$phase;
+		if(phase == '$apply' || phase == '$digest') {
+			if(fn && (typeof(fn) === 'function')) {
+				fn();
+			}
+		} else {
+			this.$apply(fn);
+		}
+	};
+})
+
+.directive('datepicker', function($parse) {
+	return function(scope, element, attrs) {
 		parsed = $parse(attrs.datepicker);
 		element.pickadate({
 			firstDay: 1, // Start with a Monday
+			clear: '', //Disable the clear button
 
 			onStart: function() {
-				//Get initial date from $scope
+				//Update initial date into the scope
 				currentDate = parsed(scope); 
 				this.set('select', currentDate);
-				scope.$apply(function(){
+				scope.safeApply(function(){
 					parsed.assign(scope, currentDate);
 				});
 			},
@@ -18,32 +48,10 @@ angular.module('SimpleWorkCal', [])
 			onClose: function() {
 				// Update the scope with the latest date from the picker
 				latestDate = this.get();
-				scope.$apply(function(){
+				scope.safeApply(function(){
 					parsed.assign(scope, latestDate);
 				});
 			}
 		});
-	};
-
-	return {
-		restrict : 'A',
-		link     : linker
 	}
 })
-
-.controller('DateCtrl', function($scope) {
-	var currentDate = new Date(); // Lets begin with today!!
-	$scope.chosenDate = currentDate;
-
-	$scope.tasks = [
-		'',
-		'something here',
-		'',
-		'',
-		'',
-		'',
-		'',
-		'',
-		''
-		]
-});
