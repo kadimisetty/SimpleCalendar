9486162a448ed0c50da288807b00189bc978731e
Sri Kadimisetty <s@sri.io>
2013-10-17 07:10:18 -0500

    initiate date with current date

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,42 @@
-angular.module('SimpleWorkCal', []).
-controller('DateCtrl', function($scope) {
+angular.module('SimpleWorkCal', [])
+.directive('datepicker', function($parse) {
+
+	var linker = function(scope, element, attrs) {
+		parsed = $parse(attrs.datepicker);
+		element.pickadate({
+			onStart: function() {
+				currentDate = parsed(scope);
+				this.set('select', currentDate);
+			},
+
+			onClose: function() {
+				latestDate = this.get();
+				scope.$apply(function(){
+					parsed.assign(scope, latestDate);
+				});
+			}
+		});
+	};
+
+	return {
+		restrict : 'A',
+		link     : linker
+	}
+})
+
+.controller('DateCtrl', function($scope) {
+	var currentDate = new Date(); // Lets begin with today!!
+	$scope.chosenDate = currentDate;
+
+	$scope.tasks = [
+		'',
+		'something here',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		''
+		]
 });
